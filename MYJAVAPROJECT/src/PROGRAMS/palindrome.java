83520c66dddc50e4e79dfa85be51ecdd7fdc792a
Adarsh-S10 <singhadarshkumar388@gmail.com>
2025-02-25 12:43:39 +0530

    Daily Learning

---
+++ MYJAVAPROJECT/src/PROGRAMS/palindrome.java
@@ -0,0 +1,9 @@
+//29.	WAP TO CHECK WHETHER THE NUMBER IS PALINDROME OR NOT.->IN CLASS
+
+package PROGRAMS;
+import java.util.*;
+
+public class palindrome {
+    
+    
+}
--- MYJAVAPROJECT/src/PROGRAMS/reversenumber.java
+++ MYJAVAPROJECT/src/PROGRAMS/reversenumber.java
@@ -1,8 +1,21 @@
-package PROGRAMS;
+//28.	WAP TO REVERSE THE NUMBER.->IN CLASS
 
+package PROGRAMS;
+import java.util.*;
 public class reversenumber {
     public static void main(String[] args) {
-        
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a number :");
+int num = sc.nextInt();
+int reversed =0;
+
+    while (num>0){
+    int digit = num%10;
+    reversed=reversed*10+digit;
+    num/=10;
+ }
+ System.out.println("reversed number is :"+reversed);
+
     }
     
 }
